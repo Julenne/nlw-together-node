8a772473db2033dca40295a05b1592654fe728d5
Arlene Pelenda <arlenej011@gmail.com>
2021-06-21 17:50:14 -0300

    iniciando o typescript

---
+++ index.ts
@@ -0,0 +1,6 @@
+function enviarEmail(nome:string, email: string){
+  console.log(`Enviar email para ${nome} com email ${email}`);
+}
+
+enviarEmail('Arlene', 'arlenej011@gmail.com')
+//inicialização do typescript: yarn tsc --init
