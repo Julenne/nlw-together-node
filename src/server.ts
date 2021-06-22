285c9fc308de21c3a0988da5450feb5366067fe2
Arlene Pelenda <arlenej011@gmail.com>
2021-06-22 15:56:57 -0300

    Configurando o banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,7 @@
+import "reflect-metadata";
 import express from "express";
 
-//o ts-node-dev (parecido com o nodemon) é uma biblioteca que consegue fazer com que o node entenda o arquivo sem precisar duplicar e criar um arquivo .js no projeto
-// @types/express =>  o expresses tem tipagens que a gente precisa instalar com: yarn add @types/express
+import "./database";
 const app = express();
-/**
- * GET    => Buscar uma informação
- * POST   => Inserir (Criar) uma informação
- * PUT    => Alterar uma informação
- * DELETE => Remover um dado
- * PATCH  => Alterar uma informação específica
- */
-app.get('/test', (request, response) => {
-  return response.send('Olá NLW!')
-})
+
 app.listen(3000, () => console.log('Server running'));
