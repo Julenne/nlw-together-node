c7e12cdc18363d791e7c74dafe48023601097ab7
Arlene Pelenda <arlenej011@gmail.com>
2021-06-26 22:54:15 -0300

    criando listagem de tags

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { AuthenticateUserController } from "./controllers/AuthenticateUserContro
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { ListTagsController } from "./controllers/ListTagsController";
 import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
 import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
@@ -16,13 +17,15 @@ const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
 const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
 const listUserSendComplimentsController = new ListUserSendComplimentsController();
+const listTagsController = new ListTagsController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags",ensureAuthenticate, ensureAdmin, createTagController.handle);
-router.post("/login", authenticateUserController.handle)
-router.post("/compliments",ensureAuthenticate, createComplimentController.handle )
+router.post("/login", authenticateUserController.handle);
+router.post("/compliments",ensureAuthenticate, createComplimentController.handle );
 
-router.get("/users/compliments/send",ensureAuthenticate, listUserSendComplimentsController.handle)
-router.get("/users/compliments/receive",ensureAuthenticate, listUserReceiveComplimentsController.handle)
+router.get("/users/compliments/send",ensureAuthenticate, listUserSendComplimentsController.handle);
+router.get("/users/compliments/receive",ensureAuthenticate, listUserReceiveComplimentsController.handle);
+router.get("/tags",ensureAuthenticate, listTagsController.handle);
 
 export { router };
