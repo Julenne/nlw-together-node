a0dac24ab6bdd82f3f489029ccba4ee7e39fd462
Arlene Pelenda <arlenej011@gmail.com>
2021-06-26 23:05:23 -0300

    rota get all de usuários

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { ListTagsController } from "./controllers/ListTagsController";
 import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
+import { ListUserController } from "./controllers/ListUsersController";
 import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { ensureAuthenticate } from "./middlewares/ensureAuthenticate";
@@ -18,7 +19,7 @@ const createComplimentController = new CreateComplimentController();
 const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
 const listUserSendComplimentsController = new ListUserSendComplimentsController();
 const listTagsController = new ListTagsController();
-
+const listUsersController = new ListUserController();
 router.post("/users", createUserController.handle);
 router.post("/tags",ensureAuthenticate, ensureAdmin, createTagController.handle);
 router.post("/login", authenticateUserController.handle);
@@ -27,5 +28,5 @@ router.post("/compliments",ensureAuthenticate, createComplimentController.handle
 router.get("/users/compliments/send",ensureAuthenticate, listUserSendComplimentsController.handle);
 router.get("/users/compliments/receive",ensureAuthenticate, listUserReceiveComplimentsController.handle);
 router.get("/tags",ensureAuthenticate, listTagsController.handle);
-
+router.get("/users",ensureAuthenticate, listUsersController.handle)
 export { router };
